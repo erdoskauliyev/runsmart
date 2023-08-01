e9d84d8a9e96004c14b96186363676ad503afdf2
Dos <yerdoskauliyev@gmail.com>
2023-08-01 08:33:05 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1300,
+        lazyLoad: 'ondemand',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        prevArrow: '<button type="button" class="slick-prev"><</button>',
+        nextArrow: '<button type="button" class="slick-next">></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+              }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+        ]    
+      });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+  });
